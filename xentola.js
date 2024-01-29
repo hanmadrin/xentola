ff0e55c4a2d8212bc6b4889d171eb2ed04c99330
hanmadrin <hanmadrin@gmail.com>
2024-01-30 02:25:27 +0600

    test

--- xentola.js
+++ xentola.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4545;
 app.use(cors());
-app.use('/', (req, res) => { res.sendFile('./index.html', { root: __dirname }); });
+app.use('/public',express.static('./public'));
+app.use('/', (req, res) => { res.sendFile('./public/index.html', { root: __dirname }); });
 app.listen(port, "0.0.0.0",() => {
     console.log(`server running at http://localhost:${port}`)
 });
