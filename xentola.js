8433821b8e25dbecd8f6f7e05635c57f79a56379
hanmadrin <hanmadrin@gmail.com>
2024-01-30 03:49:11 +0600

    save

--- xentola.js
+++ xentola.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4545;
 app.use(cors());
-// app.use('/public',express.static('./public'));
-console.log(path.join(__dirname, 'public'));
+app.use('/public',express.static('./public'));
+// console.log(path.join(__dirname, 'public'));
 // app.use('/public', (req,res,next)=>{console.log(req.url);next()},express.static(path.join(__dirname, 'public')));
 app.use('/', (req, res) => { 
     // console requested url
-    console.log(req.url);
+    // console.log(req.url);
     res.sendFile('./public/index.html', { root: __dirname }); 
 });
 app.listen(port, "0.0.0.0",() => {
